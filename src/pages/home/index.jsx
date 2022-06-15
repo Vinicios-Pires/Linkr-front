eb911d852b50fb934d6389d2320d0e1e5ce0f8fd
Isabela <isabela_cmoreira@hotmail.com>
2022-06-15 17:19:13 -0300

    feat: changed the location of the previous homepage file

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "../../styles/global.style";
+import Header from "../../components/header"
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <S.Body> 
+        <h1>TESTE</h1>
+      </S.Body>
+    </>
+  );
+}
