6a2c3aa7183f5e25086dc43ded6c6e4fdc6aae78
Pimenta <mateuspimartins@hotmail.com>
2022-06-15 02:04:39 -0300

    Created Header and global styles

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "../styles/global.style.js";
+import Header from "../components/header.js"
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <S.Body> 
+        <h1>TESTE</h1>
+      </S.Body>
+    </>
+  );
+}
