bfeff308e563d7dc766cfaec72c123a8be829bba
Pimenta <mateuspimartins@hotmail.com>
2022-06-17 13:00:45 -0300

    Logout Arrow Up and Down OK

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react"
 import { Link } from "react-router-dom"
-import { IoIosArrowDown } from "react-icons/io"
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io"
 
 import * as S from "../styles/global.style.js"
 
 import { MenuContext } from "../contexts/menu.context.jsx"
 
 import profilePic from "../assets/default-avatar.jpg"
+
 
 export default function Header() {
   const { menuIsOpen, setMenuIsOpen } = useContext(MenuContext)
@@ -16,15 +17,41 @@ export default function Header() {
     setMenuIsOpen(!menuIsOpen)
   }
 
+  function Navbar(props) {
+    return (
+      <nav onClick={handleMenuClick}>
+        <ul>{props.children}</ul>
+      </nav>
+    )
+  }
+  
+  function NavItem(props) {
+    return <li >{props.children}</li>
+  }
+
+  function LogoutArrow(props) {
+    const { menuIsOpen } = useContext(MenuContext)
+    return <>{!menuIsOpen ? <IoIosArrowDown/> : <IoIosArrowUp/>}</>
+  }
+  
+  function NavItemHidden(props) {
+    const { menuIsOpen } = useContext(MenuContext)
+    return <>{menuIsOpen && props.children}</>
+  }
+  
+  function DropLogout(props) {
+    return <S.DropdownMenu>{props.children}</S.DropdownMenu>
+  }
+
   return (
     <S.Header>
       <Link to="/">
         <h1>linkr</h1>
       </Link>
-      <Navbar >
+      <Navbar onClick={handleMenuClick}>
         <NavItem>
-          <IoIosArrowDown onClick={handleMenuClick} />
-          {/* <IoIosArrowUp onClick={handleMenuClick}/> */}
+          <LogoutArrow>
+          </LogoutArrow>
           <NavItemHidden>
             <DropLogout>
               <span onClick={() => {}}>Logout</span>
@@ -37,25 +64,4 @@ export default function Header() {
       </Navbar>
     </S.Header>
   )
-}
-
-function Navbar(props) {
-  return (
-    <nav>
-      <ul> {props.children}</ul>
-    </nav>
-  )
-}
-
-function NavItem(props) {
-  return <li>{props.children}</li>
-}
-
-function NavItemHidden(props) {
-  const { menuIsOpen } = useContext(MenuContext)
-  return <>{menuIsOpen && props.children}</>
-}
-
-function DropLogout(props) {
-  return <S.DropdownMenu>{props.children}</S.DropdownMenu>
 }
--- src/contexts/menu.context.jsx
+++ src/contexts/menu.context.jsx
@@ -4,9 +4,10 @@ export const MenuContext = createContext()
 
 export function MenuProvider(props) {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const [logoutArrow, setLogoutArrow] = useState(false)
 
   return (
-    <MenuContext.Provider value={{ menuIsOpen, setMenuIsOpen }}>
+    <MenuContext.Provider value={{ menuIsOpen, setMenuIsOpen, logoutArrow, setLogoutArrow }}>
       {props.children}
     </MenuContext.Provider>
   )
