24f53ad1ff7f8095b7c8ae4ccc9a1fd6c27dbee3
Celso <celsobenedetti2@gmail.com>
2022-06-20 09:38:16 -0300

    feat: drop down search results on click outside

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   SearchBarWrapper,
   Input,
@@ -13,7 +13,15 @@ export default function SearchBar() {
   const [inputText, setInputText] = useState("");
   const [usersData, setUsersData] = useState([]);
 
+  const [areResultsDisplayed, setAreResultsDisplayed] = useState(false);
+  const ref = useRef();
+
   const handleInput = (e) => setInputText(e.target.value);
+
+  const displayResults = (results) => {
+    setUsersData(results);
+    setAreResultsDisplayed(results.length > 0);
+  };
 
   useEffect(() => {
     if (inputText.length > 2) {
@@ -23,15 +31,29 @@ export default function SearchBar() {
             username: inputText,
           },
         })
-        .then(({ data }) => setUsersData(data))
+        .then(({ data }) => displayResults(data))
         .catch(console.dir);
     } else {
-      setUsersData([]);
+      setAreResultsDisplayed(false);
     }
   }, [inputText]);
 
+  useEffect(() => {
+    // If the menu is open and the clicked target is not within the menu, then close the menu
+    const checkIfClickedOutside = (e) => {
+      if (areResultsDisplayed && !ref.current?.contains(e.target))
+        setAreResultsDisplayed(false);
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside); // Cleanup the event listener
+    };
+  }, [areResultsDisplayed]);
+
   return (
-    <SearchBarWrapper>
+    <SearchBarWrapper ref={ref}>
       <HideTransitionBlock />
       <Input
         minLength={3}
@@ -40,7 +62,7 @@ export default function SearchBar() {
         placeholder="Search for people"
       />
       <SearchIcon />
-      {usersData.length > 0 && (
+      {areResultsDisplayed && (
         <ResultsContainer>
           {usersData.map((user, index) => (
             <UserResult key={user.username + index}>
