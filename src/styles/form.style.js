f9879b2435742b0fa5281b32dc102e5613d04550
Pimenta <mateuspimartins@hotmail.com>
2022-06-24 18:20:28 -0300

    fix margin home

--- src/styles/form.style.js
+++ src/styles/form.style.js
@@ -12,7 +12,7 @@ export const FormWrapper2 = styled.main`
   background: #FFFFFF;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 border-radius: 16px;
-margin-left: 22px;
+margin-left: 10px;
 
 @media (max-width: 925px) {
   width: clamp(350px, 100%, 100vw);
