5205733980c0ea08bcf085723d012dbbf9c61718
Pimenta <mateuspimartins@hotmail.com>
2022-06-24 12:32:15 -0300

    hashtagposts

---
+++ src/components/PostsUser/hashtag.posts.jsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import Post from "../Posts/Post";
+import { PostsWrapper } from "../Posts/styles";
+
+export default function PostsHashtag() {
+  const { hashtag } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(hashtag)
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/timeline/${hashtag}`)
+      .then(({ data }) => setPosts(data))
+      .catch(() =>
+        window.alert(
+          "An error occured while trying to fetch the posts, please refresh the page",
+        ),
+      )
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) return <PostsWrapper>Loading...</PostsWrapper>;
+
+  return (
+    <PostsWrapper>
+      {posts.length > 0
+        ? posts.map((post, index) => (
+            <Post
+              key={index}
+              pictureUrl={post.pictureUrl}
+              username={post.username}
+              description={post.description}
+              linkData={post.linkData}
+            />
+          ))
+        : "There are no posts yet"}
+    </PostsWrapper>
+  );
+}
